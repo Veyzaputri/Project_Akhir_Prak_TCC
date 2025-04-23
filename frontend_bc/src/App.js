191fb7a5137cf1dec9bebdf6e14450d724c1c886
Shinta Nursobah Chairani <142827052+Shinta505@users.noreply.github.com>
2025-04-24 04:08:53 +0700

    Update App.js

--- frontend_bc/src/App.js
+++ frontend_bc/src/App.js
@@ -8,18 +8,20 @@ import PasienList from "./components/PasienList";
 import AddPasien from "./components/AddPasien";
 import EditPasien from "./components/EditPasien";
 import PeriksaPasien from "./components/PeriksaPasien";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginUser />} />           
-        <Route path="/register" element={<AddUser />} />     
+        <Route path="/" element={<LoginUser />} />
+        <Route path="/register" element={<AddUser />} />
         <Route path="/users" element={<UserList />} />
         <Route path="/pasien" element={<PasienList />} />
         <Route path="/add-pasien" element={<AddPasien />} />
         <Route path="/edit-pasien/:id" element={<EditPasien />} />
         <Route path="/periksa/:id" element={<PeriksaPasien />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
