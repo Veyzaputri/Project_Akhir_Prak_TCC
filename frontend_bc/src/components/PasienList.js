8b1798778a24eb4d6dae04a0913feacaf3fc5c0d
Shinta Nursobah Chairani <142827052+Shinta505@users.noreply.github.com>
2025-04-25 11:43:15 +0700

    Ubah tampilan List Pasien doang

--- frontend_bc/src/components/PasienList.js
+++ frontend_bc/src/components/PasienList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { PencilIcon, TrashIcon, ClipboardDocumentListIcon, PlusIcon, HomeIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 const PasienList = () => {
+  const navigate = useNavigate();
   const [pasien, setPasien] = useState([]);
 
   useEffect(() => {
@@ -28,56 +30,93 @@ const PasienList = () => {
   };
 
   return (
-    <div className="columns mt-5 is-centered">
-      <div className="column is-full">
-        <Link to={"add"} className="button is-success mb-3">
-          Tambah Pasien
-        </Link>
-        <table className="table is-striped is-fullwidth">
+    <div className="container mt-6">
+      <div className="level mb-4">
+        <div className="level-left">
+          <button
+            onClick={() => navigate("/dashboard")}
+            className="button is-light mr-3 is-flex is-align-items-center"
+            title="Kembali ke Dashboard"
+          >
+            <span className="icon is-small">
+              <HomeIcon className="h-5 w-5" />
+            </span>
+          </button>
+          <h1 className="title is-3">Daftar Pasien</h1>
+        </div>
+        <div className="level-right">
+          <Link to="add" className="button is-primary is-flex is-align-items-center">
+            <span className="icon is-small">
+              <PlusIcon className="h-5 w-5" />
+            </span>
+            <span>Tambah Pasien</span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="table-container">
+        <table className="table is-bordered is-striped is-hoverable is-fullwidth">
           <thead>
             <tr>
-              <th>No</th>
+              <th>#</th>
               <th>Nama</th>
               <th>Tanggal Lahir</th>
               <th>Jenis Kelamin</th>
               <th>No. Telepon</th>
               <th>Alamat</th>
-              <th style={{ width: "220px" }}>Aksi</th>
+              <th className="has-text-centered">Aksi</th>
             </tr>
           </thead>
           <tbody>
-            {pasien.map((item, index) => (
-              <tr key={item.id}>
-                <td>{index + 1}</td>
-                <td>{item.nama}</td>
-                <td>{item.tgl_lahir}</td>
-                <td>{item.gender}</td>
-                <td>{item.no_telp}</td>
-                <td>{item.alamat}</td>
-                <td>
-                  <div className="buttons">
-                    <Link
-                      to={`edit/${item.id}`}
-                      className="button is-small is-info"
-                    >
-                      Edit
-                    </Link>
-                    <Link
-                      to={`periksa/${item.id}`}
-                      className="button is-small is-warning"
-                    >
-                      Periksa
-                    </Link>
-                    <button
-                      onClick={() => deletePasien(item.id)}
-                      className="button is-small is-danger"
-                    >
-                      Done
-                    </button>
-                  </div>
+            {pasien.length > 0 ? (
+              pasien.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.nama}</td>
+                  <td>{item.tgl_lahir}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.no_telp}</td>
+                  <td>{item.alamat}</td>
+                  <td className="has-text-centered">
+                    <div className="buttons is-centered is-inline-flex">
+                      <Link
+                        to={`edit/${item.id}`}
+                        className="button is-small is-info"
+                        title="Edit"
+                      >
+                        <span className="icon is-small">
+                          <PencilIcon className="h-4 w-4" />
+                        </span>
+                      </Link>
+                      <Link
+                        to={`periksa/${item.id}`}
+                        className="button is-small is-warning"
+                        title="Periksa"
+                      >
+                        <span className="icon is-small">
+                          <ClipboardDocumentListIcon className="h-4 w-4" />
+                        </span>
+                      </Link>
+                      <button
+                        onClick={() => deletePasien(item.id)}
+                        className="button is-small is-success"
+                        title="Selesai"
+                      >
+                        <span className="icon is-small">
+                          <CheckIcon className="h-4 w-4" />
+                        </span>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="7" className="has-text-centered has-text-grey">
+                  Tidak ada data pasien.
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
